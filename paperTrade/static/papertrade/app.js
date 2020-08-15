4107ccc2e6b8b5187e829af5bc26934cecfa18ee
Pablo H. Curbelo <curbelo@MacBook-Pro.local>
2020-08-15 16:54:17 -0400

    search section implemented

--- paperTrade/static/papertrade/app.js
+++ paperTrade/static/papertrade/app.js
@@ -1,3 +1,5 @@
+const token = 'bsngj7nrh5r9m81e0chg'
+
 document.addEventListener('DOMContentLoaded', () => {
 
     // Use menu links to toggle sections
@@ -6,12 +8,8 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#transactions-link').addEventListener('click', () => load_section('transactions-section'));
     
     // Load search section by default
-    load_section('search-section')
-
-
-    
-
-
+    load_section('search-section');
+    document.querySelector('#search-btn').addEventListener('click', search_stock);
 });
    
 function load_section(section) {
@@ -25,8 +23,40 @@ function load_section(section) {
     document.querySelector(`#${section}`).style.display = 'block';
 
     // Update sections when shown
+    // Update balance when changing sections
     // if secttion == dash , if sections == tran
 
 }
 
-// APIs Quote and company Profile2
+function search_stock() {
+    let symbol = document.querySelector('#search-input').value.toUpperCase();
+    const invalid = document.querySelector('.invalid-search');
+    const valid = document.querySelectorAll('.valid-search');
+    fetch(`https://finnhub.io/api/v1/stock/profile2?symbol=${symbol}&token=${token}`)
+    .then(res => res.json())
+    .then(data => {
+
+        if (data.name) {
+            invalid.style.display = 'none'
+            document.querySelector('#symbol-heading').innerHTML = symbol;
+            document.querySelector('#company-heading').innerHTML = data.name;
+
+            valid.forEach (element => {
+                element.style.display = 'block';
+            })
+
+        } else {
+            invalid.style.display = 'block';
+            valid.forEach (element => {
+                element.style.display = 'none';
+            })
+        }
+    })
+}
+
+async function stock_price(symbol) {
+    symbol = symbol.toUpperCase()
+    let response = await fetch(`https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${token}`);
+    let data = await response.json()
+    return data.c
+}
